1b8da07bc47e684c49b2bfed5fc154371892e1f5
GermanTs <giorgi.germanashvili.1@btu.edu.ge>
2022-12-08 22:47:11 +0400

    Initial commit

---
+++ app/src/main/java/ge/germants/usersapp/EditFragment.kt
@@ -0,0 +1,20 @@
+package ge.germants.usersapp
+
+import android.widget.Toast
+import androidx.fragment.app.activityViewModels
+import ge.germants.usersapp.databinding.LayoutEditFragmentBinding
+
+class EditFragment : BaseFragment<LayoutEditFragmentBinding>(LayoutEditFragmentBinding::inflate) {
+
+    private val viewModel: MainViewModel by activityViewModels()
+
+    override fun start() {
+        binding.btnSave.setOnClickListener {
+            if (binding.etName.text.toString().isNotEmpty()) {
+                viewModel.save(binding.etName.text.toString())
+            } else {
+                Toast.makeText(requireContext(), "Input is clear", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/ge/germants/usersapp/UserModule.kt
@@ -0,0 +1,19 @@
+package ge.germants.usersapp
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object UserModule {
+
+    @Provides
+    @Singleton
+    fun provideUsersRepository(): UserRepository = UserRepository.Base()
+
+    @Provides
+    fun provideCommunication(): Communication = Communication.Base()
+}
